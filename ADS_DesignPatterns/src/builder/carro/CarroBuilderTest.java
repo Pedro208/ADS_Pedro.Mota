f400c93b8fb851e5172da79b49ebeb9a9be21fbd
Pedro Augusto <pedrodaluzmota208@gmail.com>
2022-10-10 19:55:37 -0300

    commit

--- ADS_DesignPatterns/src/builder/carro/CarroBuilderTest.java
+++ ADS_DesignPatterns/src/builder/carro/CarroBuilderTest.java
@@ -1,12 +1,7 @@
 package builder.carro;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
 class CarroBuilderTest {
 
-	@Test
 	void deveContruirUmCarroPeloBuilder() {
 		Porta portaMotorista = new Porta();
 		Porta portaPassageiro = new Porta();
