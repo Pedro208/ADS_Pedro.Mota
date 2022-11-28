8e2b438224ae3b087dabdee7a70c5dc345e88bcf
Pedro Augusto <pedrodaluzmota208@gmail.com>
2022-11-28 20:37:42 -0300

    SOA Client

---
+++ web/ADS_Padrao_SOA_Client/src/main/java/br/inatel/labs/labds/padraosoa/client/AdsPadraoSoaClientApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.labs.labds.padraosoa.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsPadraoSoaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsPadraoSoaClientApplication.class, args);
+	}
+
+}
